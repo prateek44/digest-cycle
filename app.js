3257d3bd6dd51e93bcab4b5fe75b9826e51e1a6a
prateek44 <44beastmodeon@gmail.com>
2018-05-19 16:06:58 +0530

    digest cycle app

--- app.js
+++ app.js
@@ -2,19 +2,23 @@
 {
   'use strict';
   angular.module('digestCycleApp',[])
-  .controller('digestCycleAppController',['$scope','$filter',digestCycleAppController);
+  .controller('digestCycleAppController',['$scope','$filter',digestCycleAppController]);
   function digestCycleAppController($scope,$filter)
   {
     $scope.cnt=0;
     $scope.incrementer=function()
     {
-      $scope.cnt++;
-      console.log($scope);
+      $scope.cnt=1;
+      console.log("watchers #",$scope.$$watchersCount);
     };
-    $scope.watch('cnt',function(oldVal,newVal)
-  {
-    console.log("old val#",oldVal);
-    console.log("new val#",newVal);
-  })
-  }
+  //   $scope.$watch('cnt',function(newVal,oldVal)
+  // {
+  //   console.log("old val#",oldVal);
+  //   console.log("new val#",newVal);
+  // });
+  $scope.$watch(function()
+{
+  console.log("digest loop fired");
+});
+}
 })();
